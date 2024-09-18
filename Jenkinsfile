8a3336fdc53842a9c05a894a887e3da3c6aa5b9a
Robert <roberttemta@gmail.com>
2024-09-18 13:46:33 -0500

    AMI modified

--- Jenkinsfile
+++ Jenkinsfile
@@ -21,9 +21,16 @@ pipeline{
                 sh 'terraform validate -no-color'
             }
         }
+
         stage('Terraform plan'){
             steps{
                 sh 'terraform plan -no-color'
+            }
+        }
+
+        stage('Terraform apply'){
+            steps{
+                sh 'terraform apply --auto-approve -no-color'
             }
         }
     }
--- outputs.tf
+++ outputs.tf
@@ -5,13 +5,18 @@ output "public-ip" {
 
 
 output "ssh" {
+  value = "ssh -i ${aws_instance.server1.key_name}.pem ec2-user@${aws_instance.server1.public_ip}"
+
+}
+
+/*
+output "ssh" {
   value = "ssh -i ${aws_instance.server1.key_name}.pem ubuntu@${aws_instance.server1.public_ip}"
 
 }
 
 
 
-/*
 output "ssh" {
   value = [
     for instance in aws_instance.server1 : "ssh -i virginiakey.pem ec2-user@aws_instance.server1[*].public_ip"
