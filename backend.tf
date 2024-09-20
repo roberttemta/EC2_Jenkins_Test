494db9dc586ea468e026ab100d0d4405acb989c6
Robert <roberttemta@gmail.com>
2024-09-19 21:35:44 -0500

    Agent added on Jenkins

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "utc-app1-backend-w7"
-    key            = "Ec2-Jenkins-test-job/terrform.tfstate"
+    key            = "Ec2-Jenkins-test-job/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "utc-app1-locktable"
